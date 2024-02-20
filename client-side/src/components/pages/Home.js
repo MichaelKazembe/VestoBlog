90e587f4deba4a39f825f5078f097305bf5e51ea
Michaelkazembe <Michaelkazembe@gmail.com>
2024-02-20 18:09:16 +0200

    Managed to connect frontend to backend api

--- client-side/src/components/pages/Home.js
+++ client-side/src/components/pages/Home.js
@@ -1,8 +1,26 @@
 //home.js
+import React, { useState, useEffect}from 'react';
 
-import React from 'react';
+const Home = () => {
+    const [home, setHome] = useState(" ");
 
-const Home = () => <h1>Home Page</h1>;
+    useEffect(() => {
+        fetch('/home')
+        .then(response => {
+            if(response.ok) {
+                return response.text()
+                    .then(data => setHome(data));
+            }
+        })
+    }, []);
+
+    return (
+        <div> 
+            <h3>Home Page From Backend</h3>
+            <div dangerouslySetInnerHTML={{ __html: home}}></div>
+        </div>
+    );
+};
 
 export default Home;
 
