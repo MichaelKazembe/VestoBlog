c287e962012696a7ca2296a6deff242c8522bb33
Michaelkazembe <Michaelkazembe@gmail.com>
2024-02-23 12:07:04 +0200

    Login and sign up page implemented

--- client-side/src/components/pages/Login.js
+++ client-side/src/components/pages/Login.js
@@ -1,6 +1,44 @@
 //Login.js
-import React from 'react';
+import React, { Component } from "react";
+export default class Login extends Component {
+    render() {
+        return (
+            <form>
+                <h3>Sign In</h3>
+                <div className="mb-3">
+                    <label>Email address</label>
+                    <input
+                        type="email"
+                        className="form-control"
+                        placeholder="Enter email"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        placeholder="Enter password"
+                    />
+                </div>
+                <div className="mb-3">
+                    <div className="custom-control custom-checkbox">
+                        <input
+                            type="checkbox"
+                            className="custom-control-input"
+                            id="customCheck1"
+                        />
+                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                    </div>
+                </div>
+                <div className="d-grid">
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </div>
+                <p className="forgot-password text-right">
+                    Forgot <a href="#">password?</a>
+                </p>
+            </form>
+        );
+    }
+}
 
-const Login = () => <h1>Login Page</h1>;
-
-export default Login;
--- client-side/src/routes/Routes.js
+++ client-side/src/routes/Routes.js
@@ -6,6 +6,7 @@ import Contact from '../components/pages/Contact';
 import Articles from '../components/pages/Articles';
 import Login from '../components/pages/Login';
 import Register from '../components/pages/Register';
+import Profile from '../components/pages/Profile';
 import NotFound from '../components/pages/NotFound';
 
 const routes = [
@@ -13,6 +14,7 @@ const routes = [
     { path: '/about', component: <About /> },
     { path: '/contact', component: <Contact /> },
     { path: '/articles', component: <Articles />},
+    { path: '/profile', component: <Profile />},
     { path: '/register', component: <Register />},
     { path: '/login', component: <Login />},
     { path: '*', component: <NotFound /> }
