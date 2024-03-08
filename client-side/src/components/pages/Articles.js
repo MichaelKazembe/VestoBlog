e12acc5de8a482905f9831cb3b523d1254e98359
Awodele Oluwafemi <awodelefemi@yahoo.com>
2024-03-08 16:53:12 +0100

    After presentations, articles page succesfully getting data from the backend

--- client-side/src/components/pages/Articles.js
+++ client-side/src/components/pages/Articles.js
@@ -1,28 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Card from "react-bootstrap/Card";
+
 
 const Articles = () => {
-    const categories = ['Category 1', 'Category 2', 'Category 3', 'Category 4'];
+	const [posts, setPosts] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const response = await fetch("/api/home");
+                if (!response.ok) {
+                    throw new Error("Failed to fetch Posts Right now");
+                }
+                const data = await response.json();
+                setPosts(data);
+            } catch (error) {
+                setError(error.message);
+            }
+        };
+
+        fetchPosts();
+    }, []);
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
+    if (!posts) {
+        return <div>Loading...</div>;
+    }
+
 
     return (
         <div className="container">
             <div className="row">
                 <div className="col-md-8 offset-md-2">
                     <h5>Blog</h5>
+					{posts && (
+						<div>
+                        {posts.posts.map((post, index) => (
+                            <Card key={index} className="mb-3">
+                                <Card.Body>
+                                    <Card.Title>{post.title}</Card.Title>
+                                    <Card.Subtitle className="mb-2 text-muted">Author: {post.author}</Card.Subtitle>
+                                    <Card.Text>{post.content}</Card.Text>
+                                </Card.Body>
+                                <Card.Footer>
+                                    <small className="text-muted">Category: {post.category}</small><br />
+                                    <small className="text-muted">Date Posted: {post.datePosted}</small>
+                                </Card.Footer>
+                            </Card>
+                        ))}
+                    </div>
+                )}
 
-                    {categories.map((category, index) => (
-                        <div key={index}>
-                            <button className="btn btn-primary">{category}</button>
-                            <hr />
-                            <div className="card mb-4">
-                                <img src="blog-image.jpg" className="card-img-top" alt="Blog Image" />
-                                <div className="card-body">
-                                    <h2 className="card-title">{category} Title</h2>
-                                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl vitae aliquam tincidunt, urna nunc tincidunt nunc, id lacinia nisl nunc id nunc.</p>
-                                    <a href="#" className="btn btn-primary">Read More</a>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
                 </div>
             </div>
         </div>
